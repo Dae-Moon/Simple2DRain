b27103cc578053bdbf5730a9555ed74b5bd00a8e
DaeMoon <myakinin-dmitriy77@mail.ru>
2024-12-04 04:28:12 +0300

    Добавьте файлы проекта.

---
+++ s2dr_cs/Window.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace s2dr_cs
+{
+    partial class Window
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel_settings = new Panel();
+            tb_thickness = new TextBox();
+            label7 = new Label();
+            tb_angle = new TextBox();
+            label6 = new Label();
+            tb_size = new TextBox();
+            label5 = new Label();
+            tb_speed = new TextBox();
+            label4 = new Label();
+            tb_amount = new TextBox();
+            label3 = new Label();
+            tb_color_alpha = new TextBox();
+            label2 = new Label();
+            btn_color_picker = new Button();
+            pb_display = new PictureBox();
+            label1 = new Label();
+            panel_settings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pb_display).BeginInit();
+            SuspendLayout();
+            // 
+            // panel_settings
+            // 
+            panel_settings.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel_settings.BorderStyle = BorderStyle.FixedSingle;
+            panel_settings.Controls.Add(tb_thickness);
+            panel_settings.Controls.Add(label7);
+            panel_settings.Controls.Add(tb_angle);
+            panel_settings.Controls.Add(label6);
+            panel_settings.Controls.Add(tb_size);
+            panel_settings.Controls.Add(label5);
+            panel_settings.Controls.Add(tb_speed);
+            panel_settings.Controls.Add(label4);
+            panel_settings.Controls.Add(tb_amount);
+            panel_settings.Controls.Add(label3);
+            panel_settings.Controls.Add(tb_color_alpha);
+            panel_settings.Controls.Add(label2);
+            panel_settings.Controls.Add(btn_color_picker);
+            panel_settings.Location = new Point(12, 30);
+            panel_settings.Name = "panel_settings";
+            panel_settings.Size = new Size(140, 519);
+            panel_settings.TabIndex = 0;
+            // 
+            // tb_thickness
+            // 
+            tb_thickness.Location = new Point(83, 177);
+            tb_thickness.Name = "tb_thickness";
+            tb_thickness.Size = new Size(34, 23);
+            tb_thickness.TabIndex = 12;
+            tb_thickness.Text = "1";
+            tb_thickness.TextAlign = HorizontalAlignment.Center;
+            tb_thickness.TextChanged += tb_thickness_TextChanged;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 180);
+            label7.Name = "label7";
+            label7.Size = new Size(64, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Thickness :";
+            // 
+            // tb_angle
+            // 
+            tb_angle.Location = new Point(83, 148);
+            tb_angle.Name = "tb_angle";
+            tb_angle.Size = new Size(34, 23);
+            tb_angle.TabIndex = 10;
+            tb_angle.Text = "90";
+            tb_angle.TextAlign = HorizontalAlignment.Center;
+            tb_angle.TextChanged += tb_angle_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 151);
+            label6.Name = "label6";
+            label6.Size = new Size(44, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Angle :";
+            // 
+            // tb_size
+            // 
+            tb_size.Location = new Point(83, 119);
+            tb_size.Name = "tb_size";
+            tb_size.Size = new Size(34, 23);
+            tb_size.TabIndex = 8;
+            tb_size.Text = "50";
+            tb_size.TextAlign = HorizontalAlignment.Center;
+            tb_size.TextChanged += tb_size_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 122);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Size :";
+            // 
+            // tb_speed
+            // 
+            tb_speed.Location = new Point(83, 90);
+            tb_speed.Name = "tb_speed";
+            tb_speed.Size = new Size(34, 23);
+            tb_speed.TabIndex = 6;
+            tb_speed.Text = "3";
+            tb_speed.TextAlign = HorizontalAlignment.Center;
+            tb_speed.TextChanged += tb_speed_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Speed :";
+            // 
+            // tb_amount
+            // 
+            tb_amount.Location = new Point(83, 61);
+            tb_amount.Name = "tb_amount";
+            tb_amount.Size = new Size(34, 23);
+            tb_amount.TabIndex = 4;
+            tb_amount.Text = "100";
+            tb_amount.TextAlign = HorizontalAlignment.Center;
+            tb_amount.TextChanged += tb_amount_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 64);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Amount :";
+            // 
+            // tb_color_alpha
+            // 
+            tb_color_alpha.Location = new Point(83, 32);
+            tb_color_alpha.Name = "tb_color_alpha";
+            tb_color_alpha.Size = new Size(34, 23);
+            tb_color_alpha.TabIndex = 2;
+            tb_color_alpha.Text = "255";
+            tb_color_alpha.TextAlign = HorizontalAlignment.Center;
+            tb_color_alpha.TextChanged += tb_color_alpha_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Color alpha :";
+            // 
+            // btn_color_picker
+            // 
+            btn_color_picker.Location = new Point(3, 3);
+            btn_color_picker.Name = "btn_color_picker";
+            btn_color_picker.Size = new Size(132, 23);
+            btn_color_picker.TabIndex = 0;
+            btn_color_picker.Text = "Color";
+            btn_color_picker.UseVisualStyleBackColor = true;
+            btn_color_picker.Click += btn_color_picker_Click;
+            // 
+            // pb_display
+            // 
+            pb_display.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pb_display.Location = new Point(158, 12);
+            pb_display.Name = "pb_display";
+            pb_display.Size = new Size(614, 537);
+            pb_display.TabIndex = 1;
+            pb_display.TabStop = false;
+            pb_display.Paint += pb_display_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Settings";
+            // 
+            // Window
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 561);
+            Controls.Add(label1);
+            Controls.Add(pb_display);
+            Controls.Add(panel_settings);
+            Name = "Window";
+            ShowIcon = false;
+            Text = "Simple 2D Rain";
+            panel_settings.ResumeLayout(false);
+            panel_settings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pb_display).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel_settings;
+        private PictureBox pb_display;
+        private Label label1;
+        private Button btn_color_picker;
+        private TextBox tb_color_alpha;
+        private Label label2;
+        private TextBox tb_amount;
+        private Label label3;
+        private TextBox tb_speed;
+        private Label label4;
+        private TextBox tb_size;
+        private Label label5;
+        private TextBox tb_angle;
+        private Label label6;
+        private TextBox tb_thickness;
+        private Label label7;
+    }
+}
---
+++ s2dr_cs/Window.cs
@@ -0,0 +1,312 @@
+using System.ComponentModel;
+using System.Numerics;
+
+namespace s2dr_cs
+{
+    public partial class Window : Form
+    {
+        readonly Random rnd = new();
+
+        DateTime lastTime = DateTime.Now;
+        float frameTime, frameTimeAmount;
+        S2DRSettings settings = new();
+        List<Vector2> data = new();
+        Task currentColorDialog = null;
+
+        public Window()
+        {
+            InitializeComponent();
+
+            SetDoubleBuffered(pb_display);
+        }
+
+
+
+        public static void SetDoubleBuffered(Control c)
+        {
+            //Taxes: Remote Desktop Connection and painting
+            //http://blogs.msdn.com/oldnewthing/archive/2006/01/03/508694.aspx
+            if (SystemInformation.TerminalServerSession)
+                return;
+
+            System.Reflection.PropertyInfo aProp =
+                  typeof(Control).GetProperty(
+                        "DoubleBuffered",
+                        System.Reflection.BindingFlags.NonPublic |
+                        System.Reflection.BindingFlags.Instance);
+            aProp.SetValue(c, true, null);
+        }
+
+        Vector2 RandomVector(Vector2 min, Vector2 max)
+        {
+            float x = rnd.NextSingle(min.X, max.X);
+            float y = rnd.NextSingle(min.Y, max.Y);
+            return new(x, y);
+        }
+
+        KeyValuePair<Side, Side> DetermineTwoSides(float angle)
+        {
+            KeyValuePair<Side, Side> v = new(Side.Left, Side.Left);
+
+            if (angle == 90.0f)
+                v = new(Side.Top, Side.Top);
+            else if (angle == 180.0f)
+                v = new(Side.Right, Side.Right);
+            else if (angle == 270.0f)
+                v = new(Side.Bottom, Side.Bottom);
+            else
+            {
+                if (angle > 0.0f && angle < 90.0f)
+                    v = new(Side.Left, Side.Top);
+                else if (angle > 90.0f && angle < 180.0f)
+                    v = new(Side.Top, Side.Right);
+                else if (angle > 180.0f && angle < 270.0f)
+                    v = new(Side.Right, Side.Bottom);
+                else if (angle > 270.0f && angle < 360.0f)
+                    v = new(Side.Bottom, Side.Left);
+            }
+
+            return v;
+        }
+
+        KeyValuePair<Vector4, Vector4> GetArea(KeyValuePair<Side, Side> twoSides, float offset)
+        {
+            Vector4 first = new(-offset, -offset, -offset, Size.Height + offset);
+            Vector4 second = first;
+
+            switch (twoSides.Key)
+            {
+                case Side.Top:
+                    first.X = -offset;
+                    first.Y = -offset;
+                    first.Z = Size.Width + offset;
+                    first.W = -offset;
+                    break;
+                case Side.Right:
+                    first.X = Size.Width + offset;
+                    first.Y = -offset;
+                    first.Z = Size.Width + offset;
+                    first.W = Size.Height + offset;
+                    break;
+                case Side.Bottom:
+                    first.X = -offset;
+                    first.Y = Size.Height + offset;
+                    first.Z = Size.Width + offset;
+                    first.W = Size.Height + offset;
+                    break;
+            }
+
+            switch (twoSides.Value)
+            {
+                case Side.Top:
+                    second.X = -offset;
+                    second.Y = -offset;
+                    second.Z = Size.Width + offset;
+                    second.W = -offset;
+                    break;
+                case Side.Right:
+                    second.X = Size.Width + offset;
+                    second.Y = -offset;
+                    second.Z = Size.Width + offset;
+                    second.W = Size.Height + offset;
+                    break;
+                case Side.Bottom:
+                    second.X = -offset;
+                    second.Y = Size.Height + offset;
+                    second.Z = Size.Width + offset;
+                    second.W = Size.Height + offset;
+                    break;
+            }
+
+            return new(first, second);
+        }
+
+
+
+        void Generate()
+        {
+            frameTimeAmount += frameTime;
+
+            int amount = (int)(settings.amount * (settings.speed * frameTimeAmount));
+            if (amount > 0)
+                frameTimeAmount = 0;
+
+            for (int i = 0; i < amount; i++)
+            {
+                var twoSides = DetermineTwoSides(settings.angle.Normalized());
+                var area = GetArea(twoSides, settings.size);
+                var r1 = RandomVector(new(area.Key.X, area.Key.Y), new(area.Key.Z, area.Key.W));
+                var r2 = RandomVector(new(area.Value.X, area.Value.Y), new(area.Value.Z, area.Value.W));
+                data.Add(r1);
+                data.Add(r2);
+            }
+        }
+
+        void Update(Action<Vector2, Vector2> rain)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                var dir = settings.angle.ToDirection();
+
+                rain?.Invoke(data[i], data[i] + dir * settings.size);
+
+                Vector2 min = new(-settings.size - 1, -settings.size - 1);
+                Vector2 max = new(Size.Width + settings.size + 1, Size.Height + settings.size + 1);
+
+                var end = data[i] + dir * (settings.speed * 10 * (100 * frameTime));
+                if (end.X < min.X || end.Y < min.Y || end.X > max.X || end.Y > max.Y)
+                    data.RemoveAt(i);
+                else
+                    data[i] = end;
+            }
+        }
+
+
+
+        private void pb_display_Paint(object sender, PaintEventArgs e)
+        {
+            {
+                var currentTime = DateTime.Now;
+                frameTime = (float)(currentTime - lastTime).TotalSeconds;
+                lastTime = currentTime;
+            }
+
+            var g = e.Graphics;
+            {
+                g.Clear(Color.Black);
+
+                Generate();
+                Update((first, second) => { g.DrawLine(new Pen(settings.color, settings.thickness), first.ToPointF(), second.ToPointF()); });
+
+                g.DrawString($"{(int)(1 / frameTime)} FPS", Font, Brushes.White, 10, 10);
+            }
+
+            panel_settings.Update();
+            pb_display.Invalidate();
+        }
+       
+        private void btn_color_picker_Click(object sender, EventArgs e)
+        {
+            if (currentColorDialog != null)
+                return;
+
+            ColorDialog MyDialog = new();
+            MyDialog.AllowFullOpen = true;
+            MyDialog.ShowHelp = false;
+            MyDialog.AnyColor = true;
+            MyDialog.SolidColorOnly = false;
+            MyDialog.Color = settings.color;
+
+            currentColorDialog = Task.Run(() =>
+            {
+                // Update the text box color if the user clicks OK 
+                if (MyDialog.ShowDialog(Owner) == DialogResult.OK)
+                    settings.color = MyDialog.Color;
+
+                currentColorDialog = null;
+            });
+        }
+
+        private void tb_color_alpha_TextChanged(object sender, EventArgs e)
+        {   
+            if (int.TryParse(tb_color_alpha.Text, out int alpha))
+                settings.color = Color.FromArgb(Math.Clamp(alpha, 0, 255), settings.color);
+        }
+
+        private void tb_amount_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(tb_amount.Text, out int amount))
+                settings.amount = amount;
+        }
+
+        private void tb_speed_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(tb_speed.Text, out float speed))
+                settings.speed = speed;
+        }
+
+        private void tb_size_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(tb_size.Text, out float size))
+                settings.size = size;
+        }
+
+        private void tb_angle_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(tb_angle.Text, out float angle))
+                settings.angle = angle;
+        }
+
+        private void tb_thickness_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(tb_thickness.Text, out float thickness))
+                settings.thickness = thickness;
+        }
+    }
+
+    public enum Side
+    {
+        Left,
+        Top,
+        Right,
+        Bottom,
+    }
+
+    public struct S2DRSettings
+    {
+        public S2DRSettings()
+        {
+            color = Color.FromArgb(255, 255, 255, 255);
+            amount = 100;
+            speed = 3;
+            size = 50;
+            angle = 90;
+            thickness = 1;
+        }
+
+        public Color color;
+        public int amount;
+
+        public float speed;
+        public float size;
+        public float angle;
+        public float thickness;
+    }
+
+    public static class Utils
+    {
+        public static float NextSingle(this Random rnd, float min, float max)
+        {
+            // Perform arithmetic in double type to avoid overflowing
+            double range = (double)max - (double)min;
+            double sample = rnd.NextDouble();
+            double scaled = min + sample * range;
+            return (float)scaled;
+        }
+
+        public static float Normalized(this float angle)
+        {
+            if (angle < 0)
+            {
+                angle = angle + 360 * ((int)(angle / 360) + 1);
+            }
+            else if (angle >= 360)
+            {
+                angle = angle - 360 * ((int)(angle / 360));
+            }
+            // a = (360.0/65536) * ((int)(a*(65536/360.0)) & 65535);
+            return angle;
+        }
+
+        public static Vector2 ToDirection(this float angle)
+        {
+            angle = angle.Normalized() * ((float)MathF.PI / 180f);
+            var cos = MathF.Cos(angle);
+            var sin = MathF.Sin(angle);
+            return new(cos, sin);
+        }
+
+        public static Point ToPoint(this Vector2 vector2) => new((int)vector2.X, (int)vector2.Y);
+        public static PointF ToPointF(this Vector2 vector2) => new(vector2.X, vector2.Y);
+    }
+}
